81abd0d93275cc29e4a4d11fbf0f11a42cdf44ce
Felipe Afonso da Silva Vieira <127235032+Eng-FelipeA@users.noreply.github.com>
2023-05-26 19:36:08 -0300

    Delete exercicio01.py

--- exercicio01.py
+++
@@ -1,7 +0,0 @@
-def contar_animais(cabecas, patas):
-    coelhos = (patas - 2*cabecas)/2
-    patos = cabecas - coelhos
-    return patos, coelhos
-cabecas = int(input("Digite a quantidade de cabeças: "))
-patas = cabecas * 3 + 1
-print(contar_animais(cabecas,patas))
